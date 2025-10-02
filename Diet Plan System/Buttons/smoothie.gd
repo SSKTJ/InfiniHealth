f0bd71245ad3d5c7917e6c6ad4b4d2de2d2b4080
username1773 <satkarjuneja@gmail.com>
2025-10-02 10:28:20 +0530

    added buttons

---
+++ Diet Plan System/Buttons/smoothie.gd
@@ -0,0 +1,8 @@
+extends Panel
+
+
+func _on_button_4_pressed() -> void:
+	var bar = get_parent().get_parent().get_node("ProgressBar")
+	var tween = get_tree().create_tween()
+	tween.tween_property(bar, "value",bar.value+15,0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	pass # Replace with function body.
